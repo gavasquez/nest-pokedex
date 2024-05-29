0bb2a3fe737f4beb41b12d7662c824d40db1eabc
gavasquez <gustavovasquez15@gmail.com>
2024-05-29 16:51:45 -0500

    Implementacion patron adaptador axios get

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/pok-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
-  
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel = Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ){}
+
   async executeSeed(){
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    await this.pokemonModel.deleteMany();
+    //const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    //const insertPromisesArray = [];
+    const pokemonToInsert: {name: string, no: number}[] = [];
     data.results.forEach(({name, url})=> {
       const segments = url.split('/');
       //const no = segments[segments.length - 2]; asi tambien se puede obtener la penultima posición
       const no: number = +segments.at(-2);
-      console.log({name, no});
+      //insertPromisesArray.push(this.pokemonModel.create({name, no}));
+      //await this.pokemonModel.create({name, no}); //asi se crea un pokemon
+      pokemonToInsert.push({name, no});
     });
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    //await Promise.all(insertPromisesArray); // asi se ejecutarn todas las promesas
+    return 'Seed execute';
   }
 }
